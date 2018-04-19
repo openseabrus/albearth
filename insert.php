49612ed4ed08f4e9dcb9549ab43968a2d723acb5
l1904l <g.seabra@protonmail.com>
2018-04-19 17:30:12 +0100

    pls

--- insert.php
+++ insert.php
@@ -1,7 +1,7 @@
 <?php
 
 require("login.php");
-$mysqli= new mysqli('localhost', $username, $password, $database);
+$mysqli= new mysqli($server, $username, $password, $database);
 if ($mysqli->connect_errno) {
 echo "Failed to connect to MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
