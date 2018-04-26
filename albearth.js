480686c4fa9dd0b01ba1629960a7dbacff8b2335
l1904l <g.seabra@protonmail.com>
2018-04-26 12:58:07 +0100

    8 e 9 por fazer

--- albearth.js
+++ albearth.js
@@ -1,3 +1,6 @@
+var markers = new Array();
+var xmlmarkers = new Array();
+
 function initMap() {
     var mapOptions = {
         center: new google.maps.LatLng(38.644711, -9.235200),
@@ -10,17 +13,17 @@ function initMap() {
 
     var infoWindow = new google.maps.InfoWindow;
 
-    google.maps.event.addListener(map, 'click', function(event) {
-		var marker=new google.maps.Marker({
-				position: event.latLng, 
-				map: map,
-				title:"New Marker"
-			});
-		var form=document.getElementById("mar");
-		var latf=form.elements[0];
-		latf.value=event.latLng.lat();
-		var lngf=form.elements[1];
-		lngf.value=event.latLng.lng();
+    google.maps.event.addListener(map, 'click', function (event) {
+        var marker = new google.maps.Marker({
+            position: event.latLng,
+            map: map,
+            title: "New Marker"
+        });
+        var form = document.getElementById("mar");
+        var latf = form.elements[0];
+        latf.value = event.latLng.lat();
+        var lngf = form.elements[1];
+        lngf.value = event.latLng.lng();
     });
 
 
@@ -44,13 +47,13 @@ function initMap() {
 
     downloadUrl("xmloutdom.php", function (data) {
         var xml = data.responseXML;
-        var markers = xml.documentElement.getElementsByTagName("marker");
-        for (var i = 0; i < markers.length; i++) {
-            var name = markers[i].getAttribute("name");
-            var type = markers[i].getAttribute("type");
+        xmlmarkers = xml.documentElement.getElementsByTagName("marker");
+        for (var i = 0; i < xmlmarkers.length; i++) {
+            var name = xmlmarkers[i].getAttribute("name");
+            var type = xmlmarkers[i].getAttribute("type");
             var point = new google.maps.LatLng(
-                parseFloat(markers[i].getAttribute("lat")),
-                parseFloat(markers[i].getAttribute("lng")));
+                parseFloat(xmlmarkers[i].getAttribute("lat")),
+                parseFloat(xmlmarkers[i].getAttribute("lng")));
             var html = "<b>" + name + "</b> <br/>" + type;
             var icon = null;
             if (type == "hotel")
@@ -63,16 +66,15 @@ function initMap() {
                 icon = "http://labs.google.com/ridefinder/images/mm_20_yellow.png";
             else if (type == "policia")
                 icon = "http://labs.google.com/ridefinder/images/mm_20_white.png";
-            var marker = new google.maps.Marker({
+            markers[i] = new google.maps.Marker({
                 map: map,
                 position: point,
                 icon: icon
             });
-            bindInfoWindow(marker, map, infoWindow, html);
+            bindInfoWindow(markers[i], map, infoWindow, html);
+            bindRemove(markers[i], name);
         }
     });
-
-
 
 
 
@@ -80,7 +82,39 @@ function initMap() {
         google.maps.event.addListener(marker, 'click', function () {
             infoWindow.setContent(html);
             infoWindow.open(map, marker);
+            console.log("yes");
         });
     }
 
+    function bindRemove(marker, name) {
+        google.maps.event.addListener(marker, 'dblclick', function () {
+            var apaga = document.getElementById("del");
+            var n = apaga.elements[0];
+            n.value = name;
+            marker.setIcon("http://labs.google.com/ridefinder/images/mm_20_black.png");
+            console.log("no");
+        });
+    }
+}
+
+function checkLegend() {
+    var legenda = document.getElementById("legenda");
+    var hotel = legenda.elements[0];
+    var servicos = legenda.elements[1];
+    var policia = legenda.elements[2];
+    var restaurantes = legenda.elements[3];
+    var transportes = legenda.elements[4];
+
+    for (var i = 0; i < markers.length; i++) {
+        if (xmlmarkers[i].getAttribute("type")[0] == "h")
+            markers[i].setVisible(hotel.checked);
+        else if (xmlmarkers[i].getAttribute("type")[0] == "r")
+            markers[i].setVisible(restaurantes.checked);
+        else if (xmlmarkers[i].getAttribute("type")[0] == "s")
+            markers[i].setVisible(servicos.checked);
+        else if (xmlmarkers[i].getAttribute("type")[0] == "p")
+            markers[i].setVisible(policia.checked);
+        else if (xmlmarkers[i].getAttribute("type")[0] == "t")
+            markers[i].setVisible(transportes.checked);
+    }
 }
