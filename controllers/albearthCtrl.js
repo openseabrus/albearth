f4d1d90e55b49af029f686936069c8a1e802e8a3
l1904l <g.seabra@protonmail.com>
2018-05-14 22:34:53 +0100

    Added filtering interface. Needs styling

--- controllers/albearthCtrl.js
+++ controllers/albearthCtrl.js
@@ -1,4 +1,13 @@
-angular.module('albearth').controller('albearthCtrl', function ($scope, $http, $window) {
+angular.module('albearth', ['rzModule']).controller('albearthCtrl', function ($scope, $http, $window) {
+    $scope.openTime = 0;
+    $scope.closeTime = 24;
+    $scope.slider = {
+        options: {
+            floor: 0,
+            ceil: 24,
+            step: 1
+        }
+    };
 
     var setFields = function (loggedIn, username, email, picture) {
         $scope.loggedIn = loggedIn ? loggedIn : false;
@@ -21,18 +30,44 @@ angular.module('albearth').controller('albearthCtrl', function ($scope, $http, $
         $window.Cookies.set('loggedUser', user);
     }
 
-    $scope.checkCookies = function() {
+    $scope.checkCookies = function () {
         var c = $window.Cookies.getJSON('loggedUser');
-        if(c) {
+        if (c) {
             $scope.logon(c["username"], c["email"], c["picture"]);
         }
 
     }
 
-    $scope.logout = function() {
+    $scope.logout = function () {
         $scope.loggedIn = false;
         $scope.picture = "";
         $window.Cookies.remove('loggedUser');
     }
+
+    $scope.noise = {
+        options: ["Alto", "Baixo"],
+        selected: "Alto"
+    }
+
+    $scope.rating = {
+        options: [1, 2, 3, 4, 5],
+        selected: [true, false, false, false, false],
+        stars: 1
+    }
+    $scope.rate = function (star) {
+        $scope.rating.stars = star;
+        var i;
+        for (i = 0; i < $scope.rating.options.length; i++) {
+            if (i < star)
+                $scope.rating.selected[i] = true;
+            else
+                $scope.rating.selected[i] = false;
+        }
+        console.log($scope.rating.stars);
+    }
+
+    /*
+    <select name="noise" id="noiseSelect" ng-options="option.name for option in noise.options track by option.name" ng-model="noise.selected"></select>
+    */
 
 });
