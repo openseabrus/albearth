68101f0604b4fd637036f310c621496343332dc9
l1904l <g.seabra@protonmail.com>
2018-05-15 19:14:37 +0100

    Filtering working (except by rating).

--- controllers/albearthCtrl.js
+++ controllers/albearthCtrl.js
@@ -1,13 +1,4 @@
 angular.module('albearth', ['rzModule']).controller('albearthCtrl', function ($scope, $http, $window) {
-    $scope.openTime = 0;
-    $scope.closeTime = 24;
-    $scope.slider = {
-        options: {
-            floor: 0,
-            ceil: 24,
-            step: 1
-        }
-    };
 
     var setFields = function (loggedIn, username, email, picture) {
         $scope.loggedIn = loggedIn ? loggedIn : false;
@@ -44,30 +35,9 @@ angular.module('albearth', ['rzModule']).controller('albearthCtrl', function ($s
         $window.Cookies.remove('loggedUser');
     }
 
-    $scope.noise = {
-        options: ["Muito Alto", "Alto", "Moderado", "Baixo", "Muito Baixo"],
-        selected: "Moderado"
-    }
+    //##############################
+    //####### MAP MANAGEMENT #######
+    //##############################
 
-    $scope.rating = {
-        options: [1, 2, 3, 4, 5],
-        selected: [true, false, false, false, false],
-        stars: 1
-    }
-    $scope.rate = function (star) {
-        $scope.rating.stars = star;
-        var i;
-        for (i = 0; i < $scope.rating.options.length; i++) {
-            if (i < star)
-                $scope.rating.selected[i] = true;
-            else
-                $scope.rating.selected[i] = false;
-        }
-        console.log($scope.rating.stars);
-    }
-
-    /*
-    <select name="noise" id="noiseSelect" ng-options="option.name for option in noise.options track by option.name" ng-model="noise.selected"></select>
-    */
-
+    
 });
